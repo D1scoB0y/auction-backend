96c301e4bcb6d4982b801cc1828a42afa4c16d33
discoboy <jyjyartem@gmail.com>
2023-12-09 23:22:19 +0300

    Release # 3 Fixes # 8 (celery worker health check fix)

--- run_commands/celery-worker.sh
+++ run_commands/celery-worker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-celery -A src.celery_:celery worker --loglevel=info --max-memory-per-child 12000 --concurrency 2
+celery -A src.celery_:celery worker --loglevel=info --max-memory-per-child 12000 --concurrency 1
