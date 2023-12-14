17b946b5d84fce3a5c960786a89d3441b4bbd572
discoboy <jyjyartem@gmail.com>
2023-12-15 00:35:39 +0300

    Infrastructure release # 1 (monitoring by cadvisor, custom nginx exprter and node exporter)

--- run_commands/gunicorn.sh
+++ run_commands/gunicorn.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+alembic stamp head
+alembic revision --autogenerate
+alembic upgrade head
+
 gunicorn src.main:app -w 2 --worker-class uvicorn.workers.UvicornWorker -b "0.0.0.0:8000" --log-level info
